690f5bc1ce272e221429b339ffc86e34ab13a710
navaneethgold <navaneethabs.2006@gmail.com>
2025-05-29 08:34:34 +0530

    sorting

--- sorting/Insertion_sort.java
+++ sorting/Insertion_sort.java
@@ -1,7 +1,4 @@
-import java.util.*;
-
-
-public class Sorting {
+public class Insertion_sort {
    public static void printArray(int arr[]) {
        for(int i=0; i<arr.length; i++) {
            System.out.print(arr[i]+" ");
